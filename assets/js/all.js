94fe926cd12ad83b81da4a0e54166b36f9c5f8c9
SatarKuo <satarkuo@gmail.com>
2025-07-18 23:59:29 +0800

    project week2 init

---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $.get("header.html", function (data) {
+    $("#header").html(data);
+  });
+
+  $.get("footer.html", function (data) {
+    $("#footer").html(data);
+  });
+});
